9050c31b21567be7d1230f6c8364e0034e624f29
Delbighily <d.elbighily@gmail.com>
2024-05-18 17:35:22 +0300

    First commit of sign in page

---
+++ tests/SignIn.spec.js
@@ -0,0 +1,21 @@
+const {test, expect} =require('@playwright/test');
+test('Test sign in',async({page})=>
+{
+await page.goto('https://magento.softwaretestingboard.com/');
+
+//Locators
+const SignInPageBTN=page.getByRole('link', { name: 'Sign In' });
+const MailField=page.getByLabel('Email', { exact: true });
+const Password=page.getByLabel('Password');
+const SignInBTN=page.getByRole('button', { name: 'Sign In' });
+const SuccessMSG=page.getByRole('banner').getByText('Welcome, Diaa Elbighily!');
+
+//Interactions
+await SignInPageBTN.click();
+await MailField.fill('ayhaga6@gmail.com');
+await Password.fill('Qwerty@123456');
+await SignInBTN.click();
+
+//Assertions
+await expect(page.getByRole('banner')).toBeVisible();
+});
