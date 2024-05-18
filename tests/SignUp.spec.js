685e23e4f321fcf32c7ca50c25d0effb47ae398f
Delbighily <d.elbighily@gmail.com>
2024-05-18 16:44:38 +0300

    push for editing the locators

--- tests/SignUp.spec.js
+++ tests/SignUp.spec.js
@@ -1,24 +1,25 @@
-const {test} =require('@playwright/test');
+const {test, expect} =require('@playwright/test');
 test.only('First Playwright test',async({page})=>
 {
     await page.goto("https://magento.softwaretestingboard.com/");
 
     //Locators:
     const signUpLink=page.getByText('Create an Account').first();
-    const firstNameField=page.locator('#firstname');
-    const lastNameField=page.locator('#lastname');
-    const emailField=page.locator('#email_address');
-    const passwordField=page.locator('#password');
-    const passwordConfField=page.locator('#password-confirmation');
-    const submitBTN=page.locator('button.action.primary')
-    
+    const firstNameField=page.getByLabel('First Name');
+    const lastNameField=page.getByLabel('Last Name');
+    const emailField=page.getByLabel('Email', { exact: true });
+    const passwordField=page.getByRole('textbox', { name: 'Password*', exact: true })
+    const passwordConfField=page.getByLabel('Confirm Password');
+    const submitBTN=page.getByRole('button', { name: 'Create an Account' });
+    const successMSG=page.getByText('Thank you for registering')
     
     
     await signUpLink.click();
     await firstNameField.fill("Diaa");
     await lastNameField.fill("Elbighily");
-    await emailField.fill("ayhaga1@gmail.com");
+    await emailField.fill("ayhaga6@gmail.com");
     await passwordField.fill("Qwerty@123456");
     await passwordConfField.fill("Qwerty@123456");
     await submitBTN.click();
+    await expect(successMSG).toBeVisible();
 });
